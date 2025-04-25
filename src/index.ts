9885c27d91164007258c957a07a08cc85d3d5a91
ANDRIA Manohisoa <manohisoanirina707@gmail.com>
2025-04-25 10:00:12 +0300

    feat: add morgan and its types as dependencies for improved logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import router from "./routes";
-
+import morgan from "morgan";
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json()); // Pour parser le JSON
 app.use("/", router);
-
+app.use(morgan("dev"));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
